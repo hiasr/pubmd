2146fbf9362df6c704eb544f32346fde46712d79
Ruben Hias <ruben.hias@techwolf.ai>
2025-01-29 21:27:10 +0100

    add axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3939").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> &'static str {
+    "ok"
 }
